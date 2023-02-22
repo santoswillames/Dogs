ecbee0e4155b13d14de3b61bbf1c318dd4c2183b
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-02-22 11:36:37 -0300

    create register page

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useContext } from "react";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
+import { Input } from "../Forms/Input";
+import { Button } from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+  const { userLogin } = useContext(UserContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const res = await fetch(url, options);
+    if (res.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title"> Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
