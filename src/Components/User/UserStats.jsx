612b2db35e1b705243bd4b538856c2edbef45bc6
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-03-10 14:26:49 -0300

    create lazy suspense

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Head from "../Helper/Head";
 import useFetch from "../../Hooks/useFetch";
 import { STATS_GET } from "../../api";
 import Loading from "../Helper/Loading";
 import Error from "../Helper/Error";
-import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 export const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -23,10 +23,10 @@ export const UserStats = () => {
 
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
